d1d422f7606ca68f2f1f0885e1a91cb07b1de106
Vasyl <Tunguska954@gmail.com>
2020-10-21 13:17:20 +0300

    Homework3, exercises 2 and 3 :wq!

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ex2
+{
+    public class Student
+    {
+        private string Name;
+
+        public virtual void Study()
+        {
+
+        }
+        public virtual void Read()
+        {
+
+        }
+        public virtual void Write()
+        {
+
+        }
+        public virtual void Relax()
+        {
+
+        }
+        public Student(string newName)
+        {
+            Name = newName;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+
+    public class GoodStudent :Student
+    {
+        public GoodStudent(string newName) : base(newName)
+        {
+            
+        }
+        public override void Study()
+        {
+            Console.Write("Excellent ");
+        }
+        public override void Read()
+        {
+            Console.Write("Good ");
+        }
+        public override void Write()
+        {
+            Console.Write("Nice ");
+        }
+        public override void Relax()
+        {
+            Console.Write("Not bad ");
+        }
+    }
+    public class BadStudent : Student
+    {
+        public BadStudent(string newName) : base(newName)
+        {
+        }
+        public override void Study()
+        {
+            Console.Write("Could be better ");
+        }
+        public override void Read()
+        {
+            Console.Write("Well but wrong books ");
+        }
+        public override void Write()
+        {
+            Console.Write("Grammar? ");
+        }
+        public override void Relax()
+        {
+            Console.Write("AMAZING ");
+        }
+    }
+
+    class Group
+    {
+        private List<Student> ListOfStudents = new List<Student>(0);
+        public Group(string name1, string name2, string name3)
+        {
+            Random rand = new Random();
+            List<string> Names = new List<string>(){ name1, name2, name3 };
+            for (int i = 0; i < 3; i++)
+            {
+                int randomBool = rand.Next(2);
+                if (randomBool == 1)
+                {
+                    GoodStudent TempStudent = new GoodStudent(Names[i]);
+                    ListOfStudents.Add(TempStudent);
+                }
+                else
+                {
+                    BadStudent TempStudent = new BadStudent(Names[i]);
+                    ListOfStudents.Add(TempStudent);
+                }
+            }
+        }
+
+        public void ShowGroup()
+        {
+            foreach (Student S in ListOfStudents)
+            {
+                Console.WriteLine(S.GetName() + " ");
+                Console.Write("Study: "); S.Study(); Console.WriteLine();
+                Console.Write("Read: "); S.Read(); Console.WriteLine();
+                Console.Write("Write: "); S.Write(); Console.WriteLine();
+                Console.Write("Relax: "); S.Relax(); Console.WriteLine();
+                Console.WriteLine("_____________________________");
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group K24 = new Group("SonyaandVasya", "Vasyl", "Sonya");
+            K24.ShowGroup();
+        }
+    }
+}
---
+++ Ex3/Ex3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex3
+{
+    class Converter
+    {
+        public Converter(double newDollarRate, double newEuroRate)
+        {
+            if (newDollarRate == 0 || newEuroRate == 0)
+            {
+                //throw new System.Exception();
+            } else
+            {
+                DollarRate = newDollarRate;
+                EuroRate = newEuroRate;
+            }
+        }
+        public double GrnToUsd(double grnAmount)
+        {
+            return grnAmount / DollarRate;
+        }
+        public double GrnToEur(double grnAmount)
+        {
+            return grnAmount / EuroRate;
+        }
+        public double EurToGrn(double eurAmount)
+        {
+            return eurAmount * EuroRate;
+        }
+        public double UsdToGrn(double usdAmount)
+        {
+            return usdAmount * DollarRate;
+        }
+        private double DollarRate;
+        private double EuroRate;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double GrnAmount = 300;
+            double EurAmount = 1700026;
+            double UsdAmount = 172;
+            
+            Converter UsdEurConverter = new Converter(27.567, 33.128);
+            
+            Console.WriteLine("{0} GRN are equal to {1} USD", GrnAmount,
+                Convert.ToDecimal(UsdEurConverter.GrnToUsd(GrnAmount)).ToString("#,##0.00"));
+            Console.WriteLine("{0} GRN are equal to {1} EUR", GrnAmount,
+                Convert.ToDecimal(UsdEurConverter.GrnToEur(GrnAmount)).ToString("#,##0.00"));
+            
+            Console.WriteLine("{0} USD are equal to {1} GRN", UsdAmount,
+                Convert.ToDecimal(UsdEurConverter.UsdToGrn(UsdAmount)).ToString("#,##0.00"));
+            Console.WriteLine("{0} EUR are equal to {1} GRN", EurAmount,
+                Convert.ToDecimal(UsdEurConverter.EurToGrn(EurAmount)).ToString("#,##0.00"));
+        }
+    }
+}
